6d7d0311c0cbc6d1b0e3cd22b547cca626db501b
filcap <phillip.capiral@gmail.com>
2017-07-30 18:01:26 -0700

    add deadline

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -3,14 +3,20 @@ pragma solidity ^0.4.4;
 contract Remittance {
     address public owner;
     address public carol;
+    
+    uint public deadline;
     bool public submitted;
     bool public released;
     
     function Remittance(
-        address _carol) 
+        address _carol,
+        uint _duration) 
     {
+        if(_duration > 25) throw;
+        
         owner = msg.sender;
         carol = _carol;
+        deadline = block.number + _duration;
     }
     
     function compare(string _a, string _b) returns (int) {
@@ -29,6 +35,22 @@ contract Remittance {
         else if (a.length > b.length) return 1;
         
         return 0;
+    }
+    
+    function reclaimEther()
+        payable
+        returns (bool)
+    {
+        if(msg.sender != owner) throw;
+        if(released) throw;
+        if(!submitted) throw;
+        if(block.number > deadline) throw;
+        if(this.balance == 0) throw;
+        
+        if(!owner.send(this.balance)) throw;
+        released = true;
+        
+        return true;
     }
     
     function submitEther()
